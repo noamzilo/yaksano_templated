5cec6042aaaf07a94aac6acc7a20bf665f6bc3e1
noam_wsl <noamzilo@gmail.com>
2025-03-06 15:43:52 -0500

    header not so wide

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -5,8 +5,9 @@
 /* eslint-disable import/extensions */
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
-import { Transition } from '@headlessui/react';
+
 import { useLocation } from 'react-router-dom';
+
 import Button from '../elements/Button';
 import BrandIcon from './BrandIcon';
 
@@ -16,8 +17,8 @@ export default function Header() {
 	const path = location.pathname;
 
 	return (
-	  <header className="header px-4 lg:px-8 py-3 lg:py-4"> {/* Reduced padding */}
-	    <div className="flex justify-between items-center">
+	  <header className="header py-3 px-4 lg:px-8"> {/* Reduced padding */}
+	    <div className="flex justify-between">
 	      <BrandIcon />
 
 	      <button className="block text-theme-blue lg:hidden focus:outline-none" onClick={() => setIsCollapse(!isCollapse)}>
@@ -28,43 +29,23 @@ export default function Header() {
 	      </button>
 	    </div>
 
-	    <ul className="hidden lg:flex text-theme-blue tracking-widest items-center space-x-5">
+	    <ul className="hidden text-theme-blue tracking-widest items-center lg:flex flex-row">
 	      <li>
-	        <Button className={`${path === '/' ? 'active-link' : ''} font-medium text-lg`} type="link" href="/">Home</Button>
+	        <Button className={`${path === '/' ? 'active-link' : ''} font-medium text-lg px-4`} type="link" href="/">Home</Button>
 	      </li>
 	      <li>
-	        <Button className={`${path === '/team' ? 'active-link' : ''} font-medium text-lg`} type="link" href="/team">Team</Button>
+	        <Button className={`${path === '/team' ? 'active-link' : ''} font-medium text-lg px-4`} type="link" href="/team">Team</Button>
 	      </li>
 	      <li>
-	        <Button className={`${path === '/projects' ? 'active-link' : ''} font-medium text-lg`} type="link" href="/projects">Projects</Button>
+	        <Button className={`${path === '/projects' ? 'active-link' : ''} font-medium text-lg px-4`} type="link" href="/projects">Projects</Button>
 	      </li>
 	      <li>
-	        <Button className="font-medium text-lg px-5 py-2 bg-theme-purple text-white rounded-full border-2 border-theme-purple hover:bg-dark-theme-purple transition duration-200"
+	        <Button className="font-medium text-lg px-6 py-2 bg-theme-purple text-white rounded-full border-2 border-theme-purple hover:bg-dark-theme-purple transition duration-200"
 	          type="link" href="/discuss-project">
 	          Discuss Project
 	        </Button>
 	      </li>
 	    </ul>
-
-	    <Transition show={isCollapse} enter="transition-opacity duration-400" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity duration-400" leaveFrom="opacity-100" leaveTo="opacity-0">
-	      <ul className="z-50 flex flex-col text-theme-blue tracking-widest my-6 absolute bg-white w-full border-b-2 border-gray-300 lg:hidden">
-	        <li className="py-2">
-	          <Button className={`${path === '/' ? 'active-link' : ''} font-medium px-10`} type="link" href="/">Home</Button>
-	        </li>
-	        <li className="py-2">
-	          <Button className={`${path === '/team' ? 'active-link' : ''} font-medium px-10`} type="link" href="/team">Team</Button>
-	        </li>
-	        <li className="py-2">
-	          <Button className={`${path === '/projects' ? 'active-link' : ''} font-medium px-10`} type="link" href="/projects">Projects</Button>
-	        </li>
-	        <li className="mx-auto my-9">
-	          <Button className="font-bold px-5 py-2 bg-theme-purple text-white rounded-full border-2 border-theme-purple hover:bg-dark-theme-purple transition duration-200"
-	            type="link" href="/discuss-project">
-	            Discuss Project
-	          </Button>
-	        </li>
-	      </ul>
-	    </Transition>
 	  </header>
 	);
 }
